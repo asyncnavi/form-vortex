1669211dae6656b9599bae9cca65faeb457d415c
Navraj Sandhu <asyncnavi@gmail.com>
2024-01-31 21:54:45 +0530

    cleanup

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,25 +1,12 @@
-import {  IconMenu2 } from '@tabler/icons-react';
 import { Avatar } from 'primereact/avatar';
-import { Button } from 'primereact/button';
-import SideDrawer from './sidebar';
-import { useState } from 'react';
 
-const Header =( ) => {
-    const [visible,setVisible] = useState(true)
-
-    const handleSideDrawer = () => setVisible(!visible)
+const Header =() => {
 
     return (
-        <header className="flex w-full border-b py-2 ">
-            <SideDrawer visible={visible} onHide={ () => handleSideDrawer()} />
+        <header className="flex w-full border-b py-2 px-1">
             <div className="container flex justify-between items-center mx-auto">
-                <div className="flex">
-                    <Button size="small" className='mr-2' onClick={handleSideDrawer}>
-                    <IconMenu2 size={18}  /> 
-                    </Button>
-
+            
                 <img src="/logo.svg" alt="" className='w-[100px]' />
-                </div>
                <Avatar label="NS" size="large"  shape="circle" className="cursor-pointer" />
             </div>
         </header>
